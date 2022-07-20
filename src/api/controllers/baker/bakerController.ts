52d8a0d147bc374ea7eb3fa95faf11b148f29963
leonardocgs <leonardocesargoes@gmail.com>
2022-07-20 12:03:54 -0300

    Fix: bakercpfchecker

--- src/api/controllers/baker/bakerController.ts
+++ src/api/controllers/baker/bakerController.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { bakerPost } from "../../services/bakerService";
+import { bakerPost } from "../../services/bakerService/bakerService";
 
 const router = express.Router();
 router.post("/", bakerPost);
---
+++ src/api/services/bakerService/bakerService.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from "express";
+
+import DatabaseError from "../../../../error/db-error/DatabaseError";
+import pool from "../../dbconfig/db";
+import Baker from "../../models/Person/Baker";
+
+const insertIntoBaker = (
+  bakerCpf: string,
+  bakerFirstName: string,
+  bakerLastName: string,
+  bakerSalary: number
+): Baker => {
+  const newBaker = new Baker(
+    bakerFirstName,
+    bakerLastName,
+    bakerCpf,
+    bakerSalary
+  );
+  return newBaker;
+};
+const insertIntoDatabase = (baker: Baker) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(new DatabaseError("Connection failed"));
+      }
+      connection.query(
+        "Insert into baker values (?,?,?,?)",
+        [
+          baker.getCpf(),
+          baker.getFirstName(),
+          baker.getLastName(),
+          baker.getSalary(),
+        ],
+        (err, response) => {
+          if (err) {
+            reject(new DatabaseError("Insert failed"));
+          }
+          resolve(response);
+          connection.release();
+        }
+      );
+    });
+  });
+};
+const bakerAlreadyExists = (bakerCpf: string) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(new DatabaseError("Connection failed"));
+      }
+      connection.query(
+        "Select COUNT(baker_cpf) as baker_total from baker where baker_cpf = ?",
+        [bakerCpf],
+        (err, response) => {
+          if (err) {
+            reject(new DatabaseError("Select failed"));
+          }
+          if (response[0].baker_total > 0) {
+            reject(new DatabaseError("Baker already exists"));
+          }
+          connection.release();
+
+          resolve(response);
+        }
+      );
+    });
+  });
+};
+export const bakerPost = async (request: Request, response: Response) => {
+  try {
+    const { bakerCpf, bakerFirstName, bakerLastName, bakerSalary } =
+      request.body;
+    const newBaker = insertIntoBaker(
+      bakerCpf,
+      bakerFirstName,
+      bakerLastName,
+      bakerSalary
+    );
+    await bakerAlreadyExists(bakerCpf);
+
+    await insertIntoDatabase(newBaker);
+    return response
+      .status(200)
+      .json({ message: "Baker inserted successfully" });
+  } catch (error) {
+    console.log("Entrou");
+    if (error instanceof DatabaseError) {
+      return response.status(500).json({ message: error.message });
+    }
+    return response.status(400).json({
+      error: error.getMessage(),
+    });
+  }
+};
