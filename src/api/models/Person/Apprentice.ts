8f480ad59a6c5a5d9920685e36ab05d7afe053e2
leonardocgs <leonardocesargoes@gmail.com>
2022-07-20 06:59:06 -0300

    Add: Apprentice

---
+++ src/api/models/Person/Apprentice.ts
@@ -0,0 +1,30 @@
+import cpfIsValid from "../../../../aux/cpfVerifier";
+import InvalidCpfError from "../../../../error/InvalidCpfError";
+import Baker from "./Baker";
+import Person from "./Person";
+
+export default class Apprentice extends Person {
+  bakerCpf: string;
+  baker?: Baker;
+  constructor(
+    firstName: string,
+    lastName: string,
+    cpf: string,
+    bakerCpf: string
+  ) {
+    super(firstName, lastName, cpf);
+    if (!bakerCpf) {
+      throw new Error("Baker cpf is null");
+    }
+    if (typeof bakerCpf !== "string") {
+      throw new Error("Baker cpf is not a string");
+    }
+    if (!cpfIsValid(bakerCpf)) {
+      throw new InvalidCpfError("Baker cpf is invalid", bakerCpf);
+    }
+    this.bakerCpf = bakerCpf;
+  }
+  getBaker() {
+    return this.baker;
+  }
+}
